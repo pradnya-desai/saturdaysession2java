e10858b19a369f94d62340cc4ebf8016902dfb82
pradnya-desai <pradnya.d10@gmail.com>
2021-02-13 13:10:03 +0530

    pull issue

--- src/sample2.java
+++ src/sample2.java
@@ -1,5 +1,8 @@
 public class sample2 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello java8");
-    }
+
+        System.out.println("changes made by pradnya in sample 2 ");    }
+
 }
